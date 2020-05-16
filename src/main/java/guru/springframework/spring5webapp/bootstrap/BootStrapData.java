a6d7cc7f661ca19886fd909976059682f5c1bfe1
Kovács Róbert <robbi.mobile@gmail.com>
2020-05-16 15:09:36 +0200

    finishedByNcoldwave

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,4 +1,68 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-public class BootStrapData {
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);    // without this line> object references an unsaved transient instance - save the transient instance before flushing
+                                                // @cascade not helps
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author author = new Author("Eric", "Evans");
+        Book book = new Book("Domain Driven Design", "234567");
+
+        author.getBooks().add(book);
+        book.getAuthors().add(author);
+        book.setPublisher(publisher);
+        publisher.getBooks().add(book);
+
+        authorRepository.save(author);
+        bookRepository.save(book);
+        publisherRepository.save(publisher);
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author author2 = new Author("Rod", "Johnson");
+        Book book2 = new Book("J2EE Development without EJB", "3456789");
+
+        author2.getBooks().add(book2);
+        book2.getAuthors().add(author2);
+        publisher.getBooks().add(book2);
+
+        authorRepository.save(author2);
+        bookRepository.save(book2);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
+    }
 }
